330e69b68748720402617a667ca8ff52eae51311
Alexey Zyukov <aalex64@ukr.net>
2021-08-15 15:20:24 +0300

    hw11-01 completed

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,32 +1,50 @@
-import '../styles.css';
-import '../sass/main.scss';
+// import '../styles.css';
+// import '../sass/main.scss';
+import colors from '../colors.json';
+
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const bodyTheme = document.querySelector('body');
+let intervalId = null;
+
+console.log(bodyTheme.backgroundColor);
+console.log("colors from import", colors);
+console.log(startBtn.attributes);
+console.log(stopBtn);
 
 
-const themeSwitchControl = document.querySelector('#theme-switch-toggle');
-console.log(themeSwitchControl);
-themeSwitchControl.addEventListener('change', onClickChangeTheme);
-themeSwitchControl.addEventListener('change', onClickChangeLocalStorage);
-restoreTheme();
+startBtn.addEventListener('click', onClickChangeThemeStart);
+stopBtn.addEventListener('click', onClickChangeThemeStop);
 
-
-function onClickChangeTheme() {
-    document.body.classList.toggle('dark-theme');
+function onClickChangeThemeStart() {
+  intervalId = setInterval((colorChange), 1000);
+  console.log("changing color")
+  startBtn.setAttribute('disabled', true);
 };
 
-function onClickChangeLocalStorage() {
-    const change = themeSwitchControl.checked
-    if (change) {
-        localStorage.setItem('theme', 'dark-theme')
-    } else {
-        localStorage.removeItem('theme');
-    }
-}
+function colorChange() {
+  const colorsArrLength = colors.length;
+  const selectColor = colors[randomIntegerFromInterval(0, colorsArrLength)];
+  bodyTheme.style.backgroundColor = selectColor;
+};
 
-function restoreTheme() {
-    const savedTheme = localStorage.getItem('theme')
-    if (savedTheme === 'dark-theme') {
-        //console.log(savedTheme);
-        document.body.classList.add('dark-theme');
-        themeSwitchControl.checked = true;
-    }
-}
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function onClickChangeThemeStop() {
+  clearInterval(intervalId);
+  console.log("stop changing color")
+  startBtn.removeAttribute('disabled');
+};
+
+
+
+// const colorsList = [
+//   '#FFFFFF',
+//   '#2196F3',
+//   '#4CAF50',
+//   '#FF9800',
+//   '#009688',
+//   '#795548',
+// ];
